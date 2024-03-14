f77bd33ed4f2006a0f5c9083b19ba58d8679ab66
Brent Laster <bclaster@nclasters.org>
2024-03-13 21:09:48 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+apt install fuse
+wget https://releases.lmstudio.ai/linux/0.2.14/beta/LM_Studio-0.2.14-beta-1.AppImage"
+chmod +x LM_Studio-0.2.14-beta-1.AppImage
+docker compose up -d
+export DISPLAY=127.0.0.1:0
+
