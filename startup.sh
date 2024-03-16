1f68529effd0799f6e4b7d67b636e528af868ca4
Brent Laster <bclaster@nclasters.org>
2024-03-16 11:19:23 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install fuse ffmpeg pciutils -y
-wget https://releases.lmstudio.ai/linux/0.2.15/beta/LM_Studio-0.2.15-beta-1.AppImage
-chmod +x LM_Studio-0.2.15-beta-1.AppImage
+wget https://releases.lmstudio.ai/linux/0.2.14/beta/LM_Studio-0.2.14-beta-1.AppImage
+chmod +x LM_Studio-0.2.14-beta-1.AppImage
 docker compose up -d
 export DISPLAY=127.0.0.1:0
-./LM_Studio-0.2.15-beta-1.AppImage
+./LM_Studio-0.2.14-beta-1.AppImage
 
