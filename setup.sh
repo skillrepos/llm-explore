5b806bd7400ca7bcca1dbfa059bde15f2e1ee1e4
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-18 11:26:17 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,5 @@ wget https://releases.lmstudio.ai/linux/0.2.15/beta/LM_Studio-0.2.15-beta-1.AppI
 mv LM_Studio-0.2.15-beta-1.AppImage LM_Studio.AppImage
 chmod +x LM_Studio.AppImage
 docker compose up -d
-
+chmod +x startLMStudio.sh
 
