ba3ef04950a536db113f992175649cf9b9bdce99
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-18 10:23:35 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install meson ninja-build ffmpeg fuse -y
+sudo apt install meson ninja-build ffmpeg fuse pciutils -y
 wget https://github.com/libfuse/libfuse/releases/download/fuse-3.16.2/fuse-3.16.2.tar.gz
 sudo tar xvf fuse-3.16.2.tar.gz
 cd fuse-3.16.2
@@ -8,6 +8,9 @@ cd build
 meson setup ..
 sudo ninja install
 sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev libasound2 -y
+cd /workspaces/llm-explore
+rm -rf fuse-3.16.2
+rm fuse-3.16.2.tar.gz
 wget https://releases.lmstudio.ai/linux/0.2.15/beta/LM_Studio-0.2.15-beta-1.AppImage
 cd /workspaces/llm-explore
 chmod +x LM_Studio-0.2.15-beta-1.AppImage
