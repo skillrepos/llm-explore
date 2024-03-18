955ae30ad0ba38c5fea00ae0a249351d0f82a085
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-18 18:12:28 -0400

    Update startLMStudio.sh

--- startLMStudio.sh
+++ startLMStudio.sh
@@ -1,2 +1,7 @@
+wget https://releases.lmstudio.ai/linux/0.2.15/beta/LM_Studio-0.2.15-beta-1.AppImage
+mv LM_Studio-0.2.15-beta-1.AppImage LM_Studio.AppImage
+chmod +x LM_Studio.AppImage
+docker compose up -d
+chmod +x startLMStudio.sh
 export DISPLAY=127.0.0.1:0
 ./LM_Studio.AppImage &
