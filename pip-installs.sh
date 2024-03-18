c1d7e3ef115989b8e1355569ffcb80ddc79bfb55
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-17 22:44:42 -0400

    Create pip-installs.sh

---
+++ pip-installs.sh
@@ -0,0 +1,9 @@
+pip install -–break-system-packages langchain
+pip install -–break-system-packages langchain-community
+pip install -–break-system-packages langchain-core
+pip install -–break-system-packageslangchain-cli
+pip install -–break-system-packages llama-cpp-python
+pip install -–break-system-packages bs4
+pip install -–break-system-packages gpt4all
+pip install -–break-system-packageschromadb
+pip install -–break-system-packageslangchainhub
