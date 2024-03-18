14acdcc34060a48b0c55d8a564a6dbe723058b56
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-18 11:23:31 -0400

    Update and rename startup.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ cd /workspaces/llm-explore
 rm -rf fuse-3.16.2
 rm fuse-3.16.2.tar.gz
 wget https://releases.lmstudio.ai/linux/0.2.15/beta/LM_Studio-0.2.15-beta-1.AppImage
-chmod +x LM_Studio-0.2.15-beta-1.AppImage
+mv LM_Studio-0.2.15-beta-1.AppImage LM_Studio.AppImage
+chmod +x LM_Studio.AppImage
 docker compose up -d
-export DISPLAY=127.0.0.1:0
-./LM_Studio-0.2.15-beta-1.AppImage &
+
 
