fee92a1b6a5c4272fcabb2a4e3b9981e245e6228
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-18 18:19:17 -0400

    Create startJan.sh

---
+++ startJan.sh
@@ -0,0 +1,5 @@
+wget https://github.com/janhq/jan/releases/download/v0.4.8/jan-linux-x86_64-0.4.8.AppImage
+mv jan-linux-x86_64-0.4.8.AppImage jan.AppImage
+chmod +x jan.AppImage
+export DISPLAY=127.0.0.1:0
+./jan.AppImage &
