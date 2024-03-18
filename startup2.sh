b81a3927b7296e0be84edf9b89021c7bf419e56b
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-17 22:37:30 -0400

    Delete startup2.sh

--- startup2.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install fuse ffmpeg pciutils -y
-wget https://github.com/janhq/jan/releases/download/v0.4.7/jan-linux-x86_64-0.4.7.AppImage 
-chmod +x jan-linux-x86_64-0.4.7.AppImage
-docker compose up -d
-export DISPLAY=127.0.0.1:0
-./jan-linux-x86_64-0.4.7.AppImage
