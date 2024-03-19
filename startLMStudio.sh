06c7a21b87dc404c01dbfd6344e7962aa339587d
techupskills <82771267+techupskills@users.noreply.github.com>
2024-03-18 21:27:07 -0400

    Update startLMStudio.sh

--- startLMStudio.sh
+++ startLMStudio.sh
@@ -3,5 +3,4 @@ mv LM_Studio-0.2.15-beta-1.AppImage LM_Studio.AppImage
 chmod +x LM_Studio.AppImage
 docker compose up -d
 chmod +x startLMStudio.sh
-export DISPLAY=127.0.0.1:0
 ./LM_Studio.AppImage &
